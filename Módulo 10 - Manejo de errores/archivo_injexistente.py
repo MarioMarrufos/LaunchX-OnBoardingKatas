bfafa9ca7d0eb036df96de514731a2bfeb055261
MarioMarrufos <launchx05710@innovaccion.mx>
2022-02-17 00:23:50 -0500

    Agregada kata Módulo 9

---
+++ Módulo 10 - Manejo de errores/archivo_injexistente.py
@@ -0,0 +1,47 @@
+# open("/path/to/mars.jpg")
+
+
+# def main():
+#     open("/path/to/mars.jpg")
+
+# if __name__ == '__main__':
+#     main()
+
+
+
+# def main():
+#     try:
+#         configuration = open('config.txt')
+#     except FileNotFoundError:
+#         print("Couldn't find the config.txt file!")
+
+
+# if __name__ == '__main__':
+#     main()
+
+
+# def main():
+#     try:
+#         configuration = open('config.txt')
+#     except Exception:
+#         print("Couldn't find the config.txt file!")
+
+
+# def main():
+#     try:
+#         configuration = open('config.txt')
+#     except FileNotFoundError:
+#         print("Couldn't find the config.txt filer!")
+#     except IsADirectoryError:
+#         print("Found config.txt but it is a directory, couldn't read it")
+
+
+def main():
+    try:
+        configuration = open('config.txt')
+    except FileNotFoundError:
+        print("Couldn't find the config.txt file!")
+    except IsADirectoryError:
+        print("Found config.txt but it is a directory, couldn't read it")
+    except (BlockingIOError, TimeoutError):
+        print("Filesystem under heavy load, can't complete reading configuration file")
